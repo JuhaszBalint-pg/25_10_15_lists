cac99e55a11c60fc593decbf1acf3e19a3ec0175
JuhaszBalint-pg <juhasz.balint@diak.szbi-pg.hu>
2025-10-15 09:09:55 +0200

    sort method

---
+++ listmethods2.py
@@ -0,0 +1,10 @@
+honapok = ['január', 'február', 'március', 'április', 'május']
+szamok = [1, 4, 6, 3, 7, 9, 20, 2]
+
+#rendezés abc sorrendbe
+honapok.sort() #nincsen visszaadási értéke
+print (honapok)
+
+#szamok novekő sorba rendezése
+szamok.sort()
+print (szamok)
